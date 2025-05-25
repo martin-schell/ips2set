4af7f27177aba97cc041502f1db99774c7b1e046
Martin Schell <fredeulennest@gmail.com>
2025-05-25 14:09:21 +0200

    Revised and tested

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This script creates an ipset from a file with IP addresses.
 
 This script expects a file with IP addresses, separated by newline.
 
-``` title="Example for IPv4"
+```sh title="Example for IPv4"
 91.189.91.46
 91.189.91.47
 185.125.190.23
@@ -20,6 +20,167 @@ This script expects a file with IP addresses, separated by newline.
 ```
 
 ## Output
+
+## Testing
+
+```sh title="testdata_2"
+# A comment
+91.189.91.46
+91.189.91.47
+185.125.190.23
+185.125.190.24
+#185.125.190.75
+```
+
+### Testcases
+
+[!TIP]
+During the test, the ipset can be monitored with `watch ipset list <SET_NAME>`.
+
+**Testcase 1**: New set
+**Command**: `./ips2set.sh -f ipv4 -s test -v` 
+**Condition for Success**:
+
+Set *test* of type hash:ip was created with the IP addresses in the file as members. 
+
+```sh title="New set"
+Name: test
+Type: hash:ip
+Revision: 6
+Header: family inet hashsize 1024 maxelem 65536 bucketsize 12 initval 0x0774606e
+Size in memory: 416
+References: 0
+Number of entries: 5
+Members:
+91.189.91.46
+91.189.91.47
+185.125.190.23
+185.125.190.24
+185.125.190.75
+```
+
+**Testcase 2**: Update of the existing set *test*
+**Command**: `./ips2set.sh -f testdata_2 -s test -v` 
+**Description**: 185.125.190.75 in file was changed to #185.125.190.75. Because the latter is not a valid IP address, 
+185.125.190.75 in set does not exists anymore in file.
+**Condition for Success**: 185.125.190.75 is deleted from set *test*.
+
+```sh title="Updated set"
+Name: test
+Type: hash:ip
+Revision: 6
+Header: family inet hashsize 1024 maxelem 65536 bucketsize 12 initval 0x0774606e
+Size in memory: 376
+References: 0
+Number of entries: 4
+Members:
+91.189.91.46
+91.189.91.47
+185.125.190.23
+185.125.190.24
+```
+
+**Testcase 3**: Update of the existing set *test*
+**Command**: `./ips2set.sh -f ipv4 -s test -v` 
+**Description**: 185.125.190.75 is new and must be added in set.
+**Condition for Success**: 185.125.190.75 is added in set *test* (see set of ipv4).
+
+**Testcase 4**: Invalid addresses in input file
+**Command**: `./ips2set.sh -f ipv4_with_invalid -s test -v` 
+**Description**: Input file contains strings which don't match with IPv4 pattern.
+**Condition for Success**: Invalid strings do not exists in `ips_in_file`.
+
+```sh title="Updated after update"
+Name: test
+Type: hash:ip
+Revision: 6
+Header: family inet hashsize 1024 maxelem 65536 bucketsize 12 initval 0x0774606e
+Size in memory: 456
+References: 0
+Number of entries: 6
+Members:
+255.255.255.255
+127.0.0.1   
+1.20.251.69   
+192.168.1.1   
+1.2.3.4
+223.244.235.136
+```
+
+```sh title="ips2set.log"
+2025-05-24 09:26:35 [INFO] Input file: ipv4s
+2025-05-24 09:26:35 [INFO] Set name: test 
+2025-05-24 09:26:35 [INFO] --- Read valid addresses from ipv4s ---
+2025-05-24 09:26:35 [INFO] Add 192.168.1.1 in array
+2025-05-24 09:26:35 [INFO] Add 127.0.0.1 in array
+2025-05-24 09:26:35 [INFO] Add 0.0.0.0 in array
+2025-05-24 09:26:35 [INFO] Add 255.255.255.255 in array
+2025-05-24 09:26:35 [INFO] Entry 256.256.256.256 in line 5 is invalid and will be ignored
+2025-05-24 09:26:35 [INFO] Entry 999.999.999.999 in line 6 is invalid and will be ignored
+2025-05-24 09:26:35 [INFO] Entry 1.2.3 in line 7 is invalid and will be ignored
+2025-05-24 09:26:35 [INFO] Add 1.2.3.4 in array
+2025-05-24 09:26:35 [INFO] Add 1.20.251.69 in array
+2025-05-24 09:26:35 [INFO] Add 223.244.235.136 in array
+2025-05-24 09:26:35 [DEBUG] ips_in_set: 
+2025-05-24 09:26:35 [DEBUG] ips_in_file: 192.168.1.1 127.0.0.1 0.0.0.0 255.255.255.255 1.2.3.4 1.20.251.69 223.244.235.136
+2025-05-24 09:26:35 [INFO] --- Add addresses in test --- 
+2025-05-24 09:26:35 [INFO] 192.168.1.1 in ipv4s added into test
+2025-05-24 09:26:35 [INFO] 127.0.0.1 in ipv4s added into test
+2025-05-24 09:26:35 [DEBUG] 0.0.0.0 in ipv4s already exists in test
+2025-05-24 09:26:35 [INFO] 255.255.255.255 in ipv4s added into test
+2025-05-24 09:26:35 [INFO] 1.2.3.4 in ipv4s added into test
+2025-05-24 09:26:35 [INFO] 1.20.251.69 in ipv4s added into test
+2025-05-24 09:26:35 [INFO] 223.244.235.136 in ipv4s added into test 
+```
+
+**Testcase 5**: Create IPv6 set
+**Command**: `./ips2set.sh -f ipv6 -s test -v -6` 
+**Condition for Success**: Set with IPv6 addresses of file.
+
+```sh title="Set"
+Name: test
+Type: hash:ip
+Revision: 6
+Header: family inet6 hashsize 1024 maxelem 65536 bucketsize 12 initval 0x2aa7d4dc
+Size in memory: 544
+References: 0
+Number of entries: 5
+Members:
+2001:67c:1562::22
+2620:2d:4000:1::30
+2620:2d:4000:1::2f
+2001:67c:1562::21
+2620:2d:4000:1::2e
+```
+
+**Testcase 6**: Invalid IPv6 addresses in file
+**Command**: `./ips2set.sh -f ipv6_with_invalid -s test -v -6` 
+**Condition for Success**: Invalid strings do not exists in `ips_in_file`.
+
+**Testcase 8**: IPv4 network addresses in file
+**Command**: `./ips2set.sh -f ipv4nets -s test -v -n` 
+**Condition for Success**: New set of type hash:net with the network addresses.
+
+```sh title="Set with network addresses"
+Name: test
+Type: hash:net
+Revision: 7
+Header: family inet hashsize 1024 maxelem 65536 bucketsize 12 initval 0x578b26f6
+Size in memory: 696
+References: 0
+Number of entries: 5
+Members:
+162.158.0.0/15
+176.0.0.0/13
+109.40.0.0/13
+103.22.200.0/22
+197.234.240.0/22
+```
+
+If these conditions are met, the command exits with status 1 without creating a set:
+
+- Input file contains IPv4- or IPv6 network addresses and command will be executed without option `-n`
+- Input file contains IPv4- and IPv6-addresses
 
 ## Further information
 
--- ips2set.sh
+++ ips2set.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash                                                                                                                                                     
-# if [ "$EUID" -ne 0 ]; then
-#   echo "This script must be run as root."
-#   exit 1
-# fi
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
 SCRIPTNAME=$(basename "$0" | cut -d'.' -f1)
 LOG_FILE="$SCRIPTNAME".log
 
+function file_ends_with_newline() {
+    [[ $(tail -c1 "$1" | wc -l) -gt 0 ]]
+}
+
 usage() {
     echo "Usage: $0"
-    echo "-f, --file FILE       Input file with IP addresses, separated by newline"
+    echo "-f, --file FILE       Input file with IP addresses, separated by newline."
     echo "-s, --set-name NAME   Name of IP set"
     echo "-n, --networks        Optional parameter: Only necessary, if input file contents network addresses.
                                 If set, then the script creates an IP set of type hash:net (default: hash:ip)."
     echo "-6                    Optional parameter: Only necessary, if input file contensts IPv6 addresses (default: IPv4)."
     echo "-h, --help            Print usage"
+    echo "-v, --verbose         Verbose output"
 }
 
 # Logging function
@@ -30,6 +35,7 @@ log() {
 # Default values
 family=inet
 type=ip
+verbose=false
 
 # Check if no parameters were passed
 if [ $# -eq 0 ]; then
@@ -50,22 +56,26 @@ while [[ $# -gt 0 ]]; do
             exit 1
         fi
         in_file="$2"
-        log "DEBUG" "Input file: $2"
+        log "INFO" "Input file: $2"
         shift 2
         ;;
     -s | --set-name)
         set_name="$2"
-        log "DEBUG" "Set name: $2"
+        log "INFO" "Set name: $2"
         shift 2
         ;;
     -n | --networks)
         type=net
-        log "DEBUG" "Type: $type"
+        log "INFO" "Type: $type"
         shift 1
         ;;
     -6)
         family="inet6"
-        log "DEBUG" "Family: $family"
+        log "INFO" "Family: $family"
+        shift 1
+        ;;
+    -v | --verbose)
+        verbose=true
         shift 1
         ;;
     -h | --help)
@@ -80,7 +90,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-IPV4_PATTERN='(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}(\/([1-9]|3[0-2]|[1-2][0-9]))*$'
+IPV4_PATTERN='^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}(\/([1-9]|3[0-2]|[1-2][0-9]))*$'
 IPV6_PATTERN='^(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|:([0-9a-fA-F]{1,4}:){1,7}|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))(\/([1-9]|[0-9][0-9]|11[0-9]|12[0-8]))*$'
 
 # Conditions for exit status 1
@@ -100,59 +110,94 @@ elif grep -E --quiet "$IPV4_PATTERN" "$in_file" && [ $family == "inet6" ]; then
     echo "Only use option \"-6\" for IPv6."
     exit 1
 ## If file contents IPv4 network addresses, except of netmask /32, and option -n is missing.
-elif grep -E --quiet '(/[1-9]|[1-2][0-9]|3[0-1])$' "$in_file" && [ $type != "net" ]; then
+elif grep -E --quiet '\/([1-9]|[1-2][0-9]|3[0-1])$' "$in_file" && [ $type != "net" ]; then
     echo "ERROR: $in_file contains IPv4 network addresses and option \"-n\" is missing."
     echo "IPv4 network addresses (except of /32) require option \"-n\"."
     exit 1
 fi
 
-# Create array with addresses
-ips_in_file=$(cat "$in_file")
-log "DEBUG" "ips_in_file: $ips_in_file"
+# Create set, if it does not exist
+ipset create "$set_name" hash:$type family $family -exist
 
-# If set does not exists
-if ! ipset list "$set_name" >& /dev/null; then
-    log "INFO" "Create $set_name"
-    ipset create "$set_name" hash:$type family $family
-    new_set=0
-else
-    new_set=1
+# Init arrays
+declare -a ips_in_file
+declare -a ips_in_set
+
+# If the file does not end with a newline, then the last line will be ignored in the while loop.
+if ! file_ends_with_newline "$in_file"
+then
+  log "INFO" "Append newline in $in_file"
+  echo "" >> "$in_file"
 fi
 
-# Add valid IP addresses in ipset
-for ip in $ips_in_file;
+log "INFO" "--- Read valid addresses from $in_file ---"
+# Create array with valid addresses
+while IFS= read -r line; do
+    ((i++))
+    if { [ "$family" == "inet" ] && [[ ! "$line" =~ $IPV4_PATTERN ]];} || \
+    { [ "$family" == "inet6" ] && [[ ! "$line" =~ $IPV6_PATTERN ]];}; then
+        log "INFO" "Entry $line in line $i is invalid and will be ignored"
+    elif [ -z "$line" ]; then
+        log "INFO" "Line $i is empty and will be ignored"
+    else
+        log "INFO" "Add $line in array"
+        ips_in_file+=("$line")
+    fi
+done <"$in_file"
+
+# Create array from addresses in set
+if [ "$family" == "inet" ]; then
+    for ip in $(ipset list "$set_name" | grep -E "$IPV4_PATTERN");
+    do
+        ips_in_set+=("$ip")
+    done
+else
+    for ip in $(ipset list "$set_name" | grep -E "$IPV6_PATTERN");
+    do
+        ips_in_set+=("$ip")
+    done
+fi
+
+if $verbose; then log "DEBUG" "ips_in_set: ${ips_in_set[*]}"; fi
+if $verbose; then log "DEBUG" "ips_in_file: ${ips_in_file[*]}"; fi
+
+log "INFO" "--- Add addresses in $set_name ---"
+# Add non-existing addresses in set
+for ip in "${ips_in_file[@]}";
 do
 
-    if ipset add -q "$set_name" "$ip" -exist; then
-        if ipset add -q "$set_name" "$ip"; then
-            ipset add -q "$set_name" "$ip"
-            log "INFO" "$ip added into $set_name"
-        else
-            log "INFO" "$ip already exists in $set_name"
-        fi
+    if ipset add -q "$set_name" "$ip"; then
+        ipset add -q "$set_name" "$ip"
+        log "INFO" "$ip in $in_file added into $set_name"
     else
-        log "ERROR" "$ip is not valid and will not be added in $set_name" | tee
+        if $verbose; then log "DEBUG" "$ip in $in_file already exists in $set_name"; fi
     fi
 
 done
 
-if ! $new_set; then
-    if [ "$family" == "inet" ]; then
-        ips_in_set=$(ipset list "$set_name" | grep -E "$IPV4_PATTERN")
-    else
-        ips_in_set=$(ipset list "$set_name" | grep -E "$IPV6_PATTERN")
-    fi
+# If set already exists, compare the addresses in the set with the addresses in the file.
+# Addresses in the set, which do not exist in the file, will be deleted from set.
+if (( "${#ips_in_set[@]}" > 0 )); then
 
-    for ip in $ips_in_set;
+    log "INFO" "--- Compare addresses in $set_name with addresses in $in_file ---"
+    
+    for ip_set in "${ips_in_set[@]}";
     do
-        case $ip in 
-        "$ips_in_file")
-            log "INFO" "$ip exists in file"
-        ;;
-        *)
-            log "INFO" "$ip does not exists in file and will be deleted from $ips_in_set"
-            # ipset delete "$ip" "$ips_in_set"
-            echo "Delete $ip"
-        esac
+        match=false
+        for ip_file in "${ips_in_file[@]}";
+        do
+            if [ "$ip_set" = "$ip_file" ]; then
+                if $verbose; then log "DEBUG" "$ip_set in $set_name exists in file"; fi
+                match=true
+                break
+            fi
+        done
+
+        if ! $match; then
+            log "INFO" "$ip_set in $set_name does not exist in file and will be deleted from $set_name"
+            ipset del "$set_name" "$ip_set"
+        fi
+
     done
+
 fi
